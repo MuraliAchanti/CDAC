cbb69efb281fb3c159805bc861650bf8de42b868
Murali Achanti <muraliachanti@Muralis-MacBook-Pro.local>
2019-02-13 15:53:34 +0530

    Assignment 5

--- C++/Week-3/3-ComplexNumbers.cc
+++ C++/Week-3/3-ComplexNumbers.cc
@@ -20,4 +20,26 @@ class Complex
         real  = x;
         imaginary = y;
     }
+    friend void AddComplex(Complex &c1,Complex &c2);
+    friend void MultiplyComplex(Complex &c1,Complex &c2);
+};
+void AddComplex(Complex &c1,Complex &c2)
+{ 
+    printf("%d + i%d\n",c1.real + c2.real,c1.imaginary + c2.imaginary); 
+    return;
+}
+void MultiplyComplex(Complex &c1,Complex &c2)
+{
+    int result1,result2;
+    result1 = ((c1.real)*(c2.real)) - ((c2.imaginary)*(c1.imaginary)); 
+    result2 = (c1.real)*(c2.imaginary) + (c1.imaginary)*(c2.real); 
+    printf("%d + i%d\n",result1,result2);
+}
+int main()
+{
+    Complex C1;
+    Complex C2(3);
+    Complex C3(4,5); 
+    AddComplex(C2,C3);
+    MultiplyComplex(C2,C3);
 }

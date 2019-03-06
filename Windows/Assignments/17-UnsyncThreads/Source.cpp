28a79a8b86a29fe9787779b5a968acb821704c24
MuraliAchanti <muralivenkata4@gmail.com>
2019-03-06 02:00:16 -0800

    Windows Program

--- Windows/Assignments/17-UnsyncThreads/Source.cpp
+++ Windows/Assignments/17-UnsyncThreads/Source.cpp
@@ -1,4 +1,49 @@
 #include<iostream>
+#include<Windows.h>
+using namespace std;
+DWORD WINAPI ThreadFunction(LPVOID lpParam);
+volatile long int balance = 0;
+int main()
+{
+	DWORD dwThreadId1, dwThreadId2;
+	HANDLE hThread1, hThread2;
+	HANDLE hThreads[2];
+	do
+	{	
+		balance = 0;
+		hThread1 = CreateThread(NULL, 0, ThreadFunction, NULL, 0, &dwThreadId1);
+		Sleep(100);
+		hThread2 = CreateThread(NULL, 0, ThreadFunction, NULL, 0, &dwThreadId2);
+		Sleep(100);
+		if (hThread1 == NULL || hThread2 == NULL)
+		{
+			cout << "Could not create Threads" << endl;
+		}
+		else
+		{
+			DWORD dwReturn;
+			//hThreads[0] = hThread1;
+			//hThreads[1] = hThread2;
+			//WaitForMultipleObjects(2, hThreads, TRUE, INFINITE);
+			//GetExitCodeThread(hThread1, &dwReturn);
+			//cout << "Exit Code Thread 1 - " << dwReturn << endl;
+			//GetExitCodeThread(hThread2, &dwReturn);
+			//cout << "Exit Code Thread 2 - " << dwReturn << endl;
+		}
+		cout << "The Final Balance is - " << balance << endl;
+	} while (balance == 200000);
+	system("pause");
+}
+DWORD WINAPI ThreadFunction(LPVOID lpParam)
+{
+	for (int i = 0; i < 100000; i++)
+	{
+		balance = balance + 1;
+	}
+	//cout << "The Balance is - " << balance << endl;
+	return 999;
+}
+/*#include<iostream>
 #include<windows.h>
 using namespace std;
 #define MAX 64
@@ -27,4 +72,4 @@ DWORD WINAPI ThreadFunction(LPVOID lpParam)
 {
 	g_x = g_x + 1;
 	return 0;
-}
+}*/
